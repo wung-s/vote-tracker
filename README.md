c1671297c07829352189e746b6154546bb6e8843
Wung <wungmathing@gmail.com>
2018-01-23 19:28:47 +0530

    Add more seed and update README with instructions

--- README.md
+++ README.md
@@ -10,6 +10,9 @@
 
 ### Seed Database
 
+**Important**: Please create the user manually from Auth0 and make the necessary
+changes to `grifts/db.go` before running the seed command
+
     $ buffalo task db:seed
 
 ## Starting the Application
@@ -17,5 +20,12 @@
     $ PORT=4000 buffalo dev
 
 If you point your browser to [http://127.0.0.1:4000](http://127.0.0.1:4000) you should see a "Welcome to Buffalo!" page.
+
+## Deploy to Heroku
+
+    $ heroku container:login
+    $ heroku container:push web
+    $ heroku run /bin/app migrate
+    $ heroku run /bin/app  task db:seed
 
 [Powered by Buffalo](http://gobuffalo.io)
--- grifts/db.go
+++ grifts/db.go
@@ -13,10 +13,31 @@ var _ = grift.Namespace("db", func() {
 		addRole("captain")
 		addRole("scrutineer")
 		addRole("manager")
+
+		// IMPORTANT: Obtain the auth0ID from Auth0 after manually creating the user
+		addUser("test300@test.com", "auth0|5a673fd95481302d50b7fca7", "manager")
 		return nil
 	})
-
 })
+
+func addUser(email string, auth0ID string, role string) {
+	u := &models.User{
+		Email:  email,
+		AuthID: auth0ID,
+	}
+
+	models.DB.Create(u)
+
+	rs := &models.Roles{}
+	models.DB.Where("name = ?", role).All(rs)
+
+	ur := &models.UserRole{
+		UserID: u.ID,
+		RoleID: (*rs)[0].ID,
+	}
+
+	models.DB.Create(ur)
+}
 
 func addRole(role string) {
 	r := &models.Role{
